6bee4083101a7d18d7fb6400838be44b6193da94
Oleksandr Rumiantsev <58187457+olekrumian@users.noreply.github.com>
2023-04-16 01:15:39 +0200

    createAccount

---
+++ account.js
@@ -0,0 +1,56 @@
+const inputs = document.querySelectorAll('input')
+const createAccountBtn = document.querySelector('#createAccount')
+const alert = document.querySelector('.alert')
+
+const getLocalStorage = (name) => {
+  return localStorage.getItem(name)
+    ? JSON.parse(localStorage.getItem(name))
+    : []
+}
+const addToLocalStorage = (name, amount) => {
+  return localStorage.setItem(name, JSON.stringify(amount))
+}
+
+function animateWithTimeout(element) {
+  element.classList.add('shake')
+
+  setTimeout(function () {
+    element.classList.remove('shake')
+  }, 820)
+}
+
+//TODO
+/* створити функцію яка перевіряє чи є бюджет в localStorage */
+//TODO
+/* якщо є то виводиться текст з попередженими данимими */
+//TODO
+/* якщо нема то запинається функиція */
+//TODO
+/* ствоить функицію яка додає аккаунт з введенимими данимими */
+//TODO
+/* якщо все працює то редереведить на головну сторінки */
+
+createAccountBtn.addEventListener('click', function (e) {
+  e.preventDefault()
+  let accountName = inputs[0].value.toLowerCase()
+  let amount = inputs[1].value
+
+  if (!accountName) {
+    animateWithTimeout(inputs[0])
+  } else if (!amount) {
+    animateWithTimeout(inputs[1])
+  } else {
+    if (getLocalStorage(accountName)) {
+      console.log('Account already exists')
+      const existValue = getLocalStorage(accountName)
+      const newItem = existValue
+        ? JSON.parse(existValue) + parseInt(amount)
+        : []
+      addToLocalStorage(accountName, newItem)
+      // window.location.href = './main.html'
+    } else {
+      console.log('Account created')
+      addToLocalStorage(accountName, parseInt(amount))
+    }
+  }
+})
