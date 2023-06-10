2da9b8a2da7fd1ff9c5b3fb4d2146dfcc20efa18
Oleksandr Rumiantsev <58187457+olekrumian@users.noreply.github.com>
2023-06-10 13:53:28 +0200

    checkbox

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ const addBtn = document.querySelector('.add-btn')
 const spendWrapper = document.querySelector('.spend-wrapper')
 const spendArrow = document.querySelector('.spend-arrow')
 const spendBtnWrapper = document.querySelector('.spend-btn-wrapper')
+const accAmount = document.querySelector('.add-account-amount')
+const localAccountTitle = document.querySelector('.local-account')
 
-const income = localStorage.getItem('income')
-const incomeObj = JSON.parse(income)
-amount.innerHTML = `₴${incomeObj.amount}`
+const checkLocalStorage = () => {
+  if (localStorage.getItem('income')) {
+    const income = localStorage.getItem('income')
+    const incomeObj = JSON.parse(income)
+    amount.innerHTML = `€${incomeObj.amount}`
+    accAmount.innerHTML = `€${incomeObj.amount}`
+    localAccountTitle.innerHTML = `${incomeObj.description}`
+  } else {
+    amount.innerHTML = `€0`
+    accAmount.innerHTML = `€0`
+    localAccountTitle.innerHTML = `No Account Created`
+  }
+}
+
+window.addEventListener('load', checkLocalStorage)
 
 addBtn.addEventListener('click', () => {
   spendWrapper.style.cssText = 'top: 0vh; z-index: 2;'
